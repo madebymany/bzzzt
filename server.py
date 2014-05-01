169cac78182b55eddebafc539bad873096b1f631
Callum <callum@madebymany.co.uk>
2014-05-01 12:41:51 +0100

    Use WebSockets instead

--- server.py
+++ server.py
@@ -2,83 +2,76 @@
 
 import tornado.ioloop
 import tornado.web
-import os
-import functools
-import datetime
+import tornado.websocket
 
 from gpiocrust import Header, OutputPin
 from tornado.options import define, options
 
 define("pin", default=8, help="output pin", type=int)
-define("press_duration", default=3.0, help="output pin", type=float)
 define("port", default=80, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode", type=bool)
 
 
-class Presses(object):
+class Button(object):
     def __init__(self):
         self._presses = set()
         self._watchers = set()
 
-    def add(self, item):
-        self._presses.add(item)
-        self._invoke_watchers("add", item)
+    @property
+    def presses(self):
+        return self._presses
 
-    def discard(self, item):
-        self._presses.discard(item)
-        self._invoke_watchers("discard", item)
+    def add_press(self, press):
+        self._presses.add(press)
+        self._invoke_watchers("add", press)
+
+    def discard_press(self, press):
+        self._presses.discard(press)
+        self._invoke_watchers("discard", press)
 
     def add_watcher(self, method):
         self._watchers.add(method)
 
     def _invoke_watchers(self, method=None, changed=None):
         for watcher in self._watchers:
-            watcher(method, changed)
-
-    def __repr__(self):
-        return repr(self._presses)
-
-    def __len__(self):
-        return len(self._presses)
+            watcher(method, changed, self.presses)
 
 
-class MainHandler(tornado.web.RequestHandler):
-    @property
-    def button_presses(self):
-        return self.application.button_presses
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    connections = set()
+    button = Button()
 
-    def get(self):
-        self.render("index.html")
+    def open(self):
+        WebSocketHandler.connections.add(self)
 
-    def post(self):
-        token = self.get_argument("token")
-        is_pressed = self.get_argument("is_pressed", None)
+    def on_message(self, is_pressed):
+        button = WebSocketHandler.button
         if is_pressed == "yes":
-            self.button_presses.add(token)
+            button.add_press(self)
         elif is_pressed == "no":
-            self.button_presses.discard(token)
-        else:
-            self.button_presses.add(token)
-            tornado.ioloop.IOLoop.instance().add_timeout(
-                datetime.timedelta(seconds=options.press_duration),
-                functools.partial(self.button_presses.discard, token))
+            button.discard_press(self)
+        for connection in WebSocketHandler.connections:
+            connection.write_message(str(len(button.presses)))
+
+    def on_close(self):
+        WebSocketHandler.button.discard_press(self)
+        WebSocketHandler.connections.remove(self)
 
 
 tornado.options.parse_command_line()
 
 app = tornado.web.Application([
-    (r"/", MainHandler)
+    (r"/", WebSocketHandler)
 ], debug=options.debug)
 
 with Header() as header:
     finger = OutputPin(options.pin, value=False)
-    app.button_presses = Presses()
 
-    @app.button_presses.add_watcher
-    def update_pin(method, changed):
+    @WebSocketHandler.button.add_watcher
+    def update_pin(m, c, presses):
         if options.debug:
-            print app.button_presses
-        finger.value = len(app.button_presses) > 0
+            print presses
+        finger.value = len(presses) > 0
 
     try:
         app.listen(options.port)
